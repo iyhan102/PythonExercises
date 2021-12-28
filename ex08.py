32930bcaf736773867fc66291e4149f17de481c0
iyhan102 <il-young.han@asml.com>
2021-12-28 16:45:52 +0900

    ch04-02

---
+++ ex08.py
@@ -0,0 +1,14 @@
+# chapter04-02
+#시퀀스형
+# 컨테이너(container: 서로 다른 자료형 입력 가능 list, tuple, collections.deque)
+# 플랫(flat: 한개의 자료형만 입력 str, bytes, bytearray, array.array, memoryview)
+# 가변(list, bytearray, array.array, memoryview, deque)
+# 불변(tuple, str, bytes)
+# 해시테이블 (hashtable)
+# 튜플->dict으로바꿀때 key의 중복이 있을때 하나로 합쳐서 만들어야할 경우에 사용될수있음
+# hashtable 이란: key에 value를 저장하는 구조 
+# dict -> 해쉬테이블 예 
+# key값을 해싱함수를 통해 -> 해쉬 주소 -> key에 대한 value위치를 찾을 수 있음. 
+
+# Dict 
+print(__builtins__.__dict__)
